2eee8f70cf9fe60f4ee2161f0ddc88c1ab17cd28
kimSanghyun <sanghyun.kim@appg.co.kr>
2023-05-04 09:38:18 +0900

    eureka

--- eureka/src/main/java/com/netflix_clone/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/netflix_clone/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.eureka.eureka;
+package com.netflix_clone.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-
 public class EurekaApplication {
 
     public static void main(String[] args) {
